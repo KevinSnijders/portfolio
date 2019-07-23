9043e969c7d6c09119c269eb6e3648b27e8e6b7f
Kevin Snijders <kevin_snijders@live.nl>
2019-07-23 14:33:34 +0200

    Added load more items functionality, some refactoring and changed the version of jquery

--- frontend/src/scripts/app.js
+++ frontend/src/scripts/app.js
@@ -1,3 +1,26 @@
+
+
 particlesJS.load('particles-js', '/frontend/build/assets/particles.json', function () {
 
+});
+
+$(document).ready(function () {
+	var sliceStart = 0;
+	var sliceEnd = 4;
+	var targetItem = ".project__item";
+	var btnLoadMore = "#btn--load-more";
+	var hidden = "hidden";
+
+	$(targetItem).slice(sliceStart,sliceEnd).show();
+	if ($(targetItem + ":" + hidden).length !== 0) {
+		$(btnLoadMore).show();
+	}
+	$(btnLoadMore).on('click', function (e) {
+		e.preventDefault();
+		$(targetItem + ":" + hidden).slice(sliceStart, sliceEnd).show();
+		if ($(targetItem + ":" + hidden).length === 0) {
+			$(btnLoadMore).fadeOut('slow');
+			$(btnLoadMore)[0].parentElement.className = hidden;
+		}
+	});
 });
