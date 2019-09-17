94ef19f50f8243595be23bb51d45cfad5b4fca5a
Kevin Snijders <kevin_snijders@live.nl>
2019-09-17 11:50:41 +0200

    Changed button to href in details

--- src/scripts/react/components/Portfolio/Details.js
+++ src/scripts/react/components/Portfolio/Details.js
@@ -8,12 +8,12 @@ const Details = ({ title, description, demo, source }) => {
       <h2 className="portfolio__title">{title}</h2>
       <p className="portfolio__details">{description}</p>
       <div className="portfolio__links">
-        <button type="button" className="btn btn--primary mr-3">
-          View demo {demo}
-        </button>
-        <button type="button" className="btn btn--secondary">
-          View source code {source}
-        </button>
+        <a href={demo} target="_blank" rel="noopener noreferrer" className="btn btn--primary mr-3">
+          View demo
+        </a>
+        <a href={source} target="_blank" rel="noopener noreferrer" className="btn btn--secondary">
+          View source code
+        </a>
       </div>
     </>
   );
