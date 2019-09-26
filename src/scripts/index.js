e6c9e48200a56c1b91bb2f71a0753db896c0b35c
Kevin Snijders <kevin_snijders@live.nl>
2019-09-26 12:40:01 +0200

    Refactored script folder structure

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import thunkMiddleware from 'redux-thunk';
-import { requestPortfolio } from '../redux/reducers';
+import { requestPortfolio } from './redux/reducers';
 import App from './components/App/App';
 
 const rootReducer = combineReducers({ requestPortfolio });
