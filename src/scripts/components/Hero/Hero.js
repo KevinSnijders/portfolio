efe6eb1d387e923a3afa641f7af1a087eb315146
Kevin Snijders <kevin_snijders@live.nl>
2019-10-08 15:15:14 +0200

    Git changed button color in hero

--- src/scripts/components/Hero/Hero.js
+++ src/scripts/components/Hero/Hero.js
@@ -16,7 +16,7 @@ const Hero = () => {
             <span className="m-fs">Web Developer</span>
           </h1>
           <Social />
-          <Button link="#" style="btn--light btn--big mr-3" text="Download my resume" />
+          <Button link="#" style="btn--primary btn--big mr-3" text="Download my resume" />
         </div>
         <div className="col col-sm-5 col-lg-3 order-1 order-sm-2 fade-in-right">
           <Image source={Photo} alt="A selfie of me" style="scale-in-center" />
