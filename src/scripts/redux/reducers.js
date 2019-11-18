e6213b68457bad0d0bc6c297b6b444298213814c
Kevin Snijders <kevin_snijders@live.nl>
2019-11-18 13:11:02 +0100

    Add theme state to redux

--- src/scripts/redux/reducers.js
+++ src/scripts/redux/reducers.js
@@ -1,5 +1,6 @@
 import { types } from './types';
 
+const initialStateTheme = 'light';
 export const requestPortfolio = (state = [], action = {}) => {
   switch (action.type) {
     case types.GET_PORTFOLIO:
@@ -12,6 +13,15 @@ export const requestPortfolio = (state = [], action = {}) => {
 export const getNetworkStatus = (state = null, action = {}) => {
   switch (action.type) {
     case types.GET_NETWORK_STATUS:
+      return action.payload;
+    default:
+      return state;
+  }
+};
+
+export const getTheme = (state = initialStateTheme, action = {}) => {
+  switch (action.type) {
+    case types.GET_THEME:
       return action.payload;
     default:
       return state;
