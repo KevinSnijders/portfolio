cde2a92d7452c41ff5b33cbcc7ad0ee3d4647b73
Kevin Snijders <kevin_snijders@live.nl>
2019-11-18 22:47:27 +0100

    Fixed small navbar

--- src/scripts/components/Header/Navigation.js
+++ src/scripts/components/Header/Navigation.js
@@ -2,10 +2,14 @@ import React from 'react';
 import Image from '../Shared/Image';
 import Logo from '../../../assets/images/logo.png';
 import Menu from './Menu';
+import PropTypes from 'prop-types';
 
 const Navigation = ({ handleToggleTheme, theme }) => {
   return (
-    <nav data-test="NavigationComponent" className="nav fade-in-top">
+    <nav
+      data-test="NavigationComponent"
+      className="nav navbar navbar-expand-lg navbar-dark fade-in-top"
+    >
       <Image source={Logo} alt="brand logo" style="nav__image" />
       <Menu handleToggleTheme={handleToggleTheme} theme={theme} />
     </nav>
@@ -13,3 +17,8 @@ const Navigation = ({ handleToggleTheme, theme }) => {
 };
 
 export default Navigation;
+
+Navigation.propTypes = {
+  theme: PropTypes.string,
+  handleToggleTheme: PropTypes.func
+};
